b34a6812281c935bea870395751e3799214f39e5
Yashvanth <yashvanth@gmail.com>
2021-07-20 20:15:42 +0530

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService{
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDto createdUser(UserDto user) {
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(user, userEntity);
+		
+		userEntity.setEncryptedpassword("test");
+		userEntity.setUserId("testUserId");
+		
+		UserEntity storedUserDetails=userRepository.save(userEntity);
+		
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
+		
+		return returnValue;
+	}
+
+}
